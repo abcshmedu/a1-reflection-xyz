7ac024af3f78ecd539066fe6ce0f6f59a6c5d32e
octav <behrmann@hm.edu>
2017-03-30 23:13:18 +0200

    Continued work on implementation

--- src/main/java/edu/hm/renderer/Renderer.java
+++ src/main/java/edu/hm/renderer/Renderer.java
@@ -4,59 +4,88 @@ package edu.hm.renderer;
 import edu.hm.RenderMe;
 
 import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
 
 public class Renderer {
 
-    private Object object;
+    /**
+     * Target object we are retrieving information from.
+     */
+    private Object target;
 
-    public Renderer(Object object) {
-        this.object = object;
+    /**
+     * Constructor, assigns our target-object.
+     * @param target
+     */
+    public Renderer(Object target) {
+        this.target = target;
     }
 
-    public Object getObject() {
-        return object;
-    }
+    /**
+     * Renders our target-object.
+     * @return rendered string
+     * @throws IllegalAccessException
+     * @throws ClassNotFoundException
+     * @throws InstantiationException
+     */
+    public String render() throws IllegalAccessException, ClassNotFoundException,
+            NoSuchMethodException, InstantiationException, InvocationTargetException {
+        Class targetClass = target.getClass();
+        String ret = "Instance of " + targetClass.getName() + ":\n";
 
-    public void setObject(Object object) {
-        this.object = object;
-    }
+        /* Get all fields of class, no matter their access*/
+        for(Field f: targetClass.getDeclaredFields()) {
 
-    public String render() throws IllegalAccessException, ClassNotFoundException, InstantiationException {
+            f.setAccessible(true);
 
-        Class objectClass = object.getClass();
-
-        Field[] fields = objectClass.getDeclaredFields();
-
-        // Class name
-        objectClass.getName();
-
-        for(Field field : fields) {
-            if(field.isAnnotationPresent(RenderMe.class)) {
-
-                // with value
-                RenderMe renderMe = field.getAnnotation(RenderMe.class);
-                String with = renderMe.with();
-                // Class found with name
-                Class arrayRenderer = Class.forName(with);
-
-                Object obj = arrayRenderer.newInstance();
-
-
-                Class type = field.getType();
-                // type name (int)
-                type.getCanonicalName();
-
-                // field name (foo)
-                field.getName();
-
-                // value of variable foo
-                field.get(getObject());
-
-
+            /* Skip fields that don't have the RenderMe-annotation*/
+            if(!f.isAnnotationPresent(RenderMe.class)) {
+                continue;
             }
+
+            /* Fieldname */
+            ret += f.getName();
+
+            /* Value of the "with"-property*/
+            String typeRendererClass = f.getAnnotation(RenderMe.class).with();
+
+            /* Check wether the "with"-Property is set */
+            if(typeRendererClass.length() > 0) {
+                /* NonPrimitiveTypeRenderer is used */
+                Class nonPrimitiveTypeRenderer = Class.forName(typeRendererClass);
+
+                /* Instantiate nptr with field-value */
+                NonPrimitiveTypeRenderer nptr =
+                        (NonPrimitiveTypeRenderer)nonPrimitiveTypeRenderer
+                                .getDeclaredConstructor(Object.class)
+                                .newInstance(f.get(target));
+
+                /* Render field */
+                nptr.render();
+            } else {
+                /* Default typeRenderer is used */
+                ret += " (" + f.getType().getCanonicalName() + "): ";
+            }
+
+            // Class found with name
+            //Class arrayRenderer = Class.forName(with);
+
+            //Object obj = arrayRenderer.newInstance();
+
+
+            /*Class type = f.getType();
+            // type name (int)
+            type.getCanonicalName();
+
+            // f name (foo)
+            f.getName();
+
+            // value of variable foo
+            f.get(target);*/
         }
 
-        return getObject().toString();
+        return ret;
+        //return getTarget().toString();
     }
 
 }
